8dfd9a22b64bed0c78af5d239f7cf96af5681d25
SEEBAKANNAN T <61025233+seebakannan@users.noreply.github.com>
2020-03-20 00:23:21 +0530

    error fixed

--- general problems/primenumber.cs
+++ general problems/primenumber.cs
@@ -1,24 +1,22 @@
 using System;
-class MainClass
-{
-    public static void Main(string[] args)
-    {
-        int name, m, i, flag = 0;
-        Console.Write("enter the number: ");
-        name = int.Parse(Console.ReadLine());
-        m = name / 2;
-        for (i = 2; i <= m; i++)
-        {
-            if (name % i == 0)
-            {
-                Console.Write("the number is not prime");
-                flag++;
-                break;
-            }
-        }
-        if (flag == 0)
-        {
-            Console.WriteLine("the number is prime");
-        }
+class MainClass {
+  public static void Main (string[] args) {
+   int num,i, a=0,b;
+   Console.WriteLine("enter the number: ");
+   num= Convert.ToInt32(Console.ReadLine());
+   for (i=1;i<=num; i++){
+
+    if(num%i==0){
+      a++;
     }
+   }
+   if(a==2){
+     Console.Write("prime number");
+
+   }
+   else{
+     Console.WriteLine("not a prime");
+   }
+
+  }
 }
